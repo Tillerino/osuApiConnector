7a4245c177ed6c81477749b1f8a767a2c4188e5e
ThePooN <thepoon@cartooncraft.fr>
2014-10-01 18:21:02 +0200

    removed useless imports
    Signed-off-by: ThePooN <thepoon@cartooncraft.fr>

--- osuApiConnector/src/main/java/org/tillerino/osuApiModel/Downloader.java
+++ osuApiConnector/src/main/java/org/tillerino/osuApiModel/Downloader.java
@@ -1,12 +1,9 @@
 package org.tillerino.osuApiModel;
 
 import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.List;
