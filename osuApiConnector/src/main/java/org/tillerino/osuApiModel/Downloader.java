0ca7d76197c5e4bd868f552959ba9b9789436c6f
Tillerino <tillmann.gaida@gmail.com>
2015-08-10 23:27:25 +0200

    Pass on SocketTimeoutException instead of making a new IOException with URL

--- osuApiConnector/src/main/java/org/tillerino/osuApiModel/Downloader.java
+++ osuApiConnector/src/main/java/org/tillerino/osuApiModel/Downloader.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.List;
@@ -128,6 +129,8 @@ public class Downloader {
 		String content;
 		try {
 			content = downloadDirect(url);
+		} catch (SocketTimeoutException e) {
+			throw e;
 		} catch (IOException e1) {
 			throw new IOException(e1.getMessage() + " for " + formURL(false, command, parameters), e1);
 		}
