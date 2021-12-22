3d73fa322a46df8e2fb5c4a194bfee3780338352
Tillmann Gaida <tillmann.gaida@gmail.com>
2021-12-22 17:56:26 +0100

    Make sure date serialization is roundtrip-compatible

--- osuApiConnector/src/main/java/org/tillerino/osuApiModel/deserializer/DateToLong.java
+++ osuApiConnector/src/main/java/org/tillerino/osuApiModel/deserializer/DateToLong.java
@@ -7,12 +7,15 @@ import java.time.format.DateTimeFormatter;
 
 import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 
 /**
- * We store datetimes as longs and use this deserializer to deserialize ISO datetimes directly to longs.
+ * We store datetimes as longs and use this deserializer to deserialize ISO datetimes directly to longs
+ * as an alternative to integer values.
  */
 public class DateToLong extends StdDeserializer<Long> {
 	public DateToLong() {
@@ -23,8 +26,13 @@ public class DateToLong extends StdDeserializer<Long> {
 
 	@Override
 	public Long deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
-		String valueAsString = p.getValueAsString();
-		return ZonedDateTime.parse(valueAsString, dateTimeFormatter).toInstant().toEpochMilli();
+		if (p.currentToken() == JsonToken.VALUE_NUMBER_INT) {
+			return p.getLongValue();
+		}
+		if (p.currentToken() == JsonToken.VALUE_STRING) {
+			return ZonedDateTime.parse(p.getValueAsString(), dateTimeFormatter).toInstant().toEpochMilli();
+		}
+		throw MismatchedInputException.from(p, _valueType, "Expected a string or an integer");
 	}
 
 }
